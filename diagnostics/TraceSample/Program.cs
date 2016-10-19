3ce49cd89b36d95e34cac127fd0a256b5e5382e2
Louis <luqingfei@126.com>
2016-10-19 15:27:52 +0800

    TraceSample

---
+++ diagnostics/TraceSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TraceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Trace.AutoFlush = true;
+            Trace.Indent();
+            Trace.WriteLine("Entering Main");
+            Console.WriteLine("Hello World.");
+            Trace.WriteLine("Exiting Main");
+            Trace.Unindent();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+/*
+ https://msdn.microsoft.com/zh-cn/library/system.diagnostics.trace(VS.90).aspx
+
+ */
